2a74e80d52dec379b00f400012e1b43987870a12
nataraj.ramamoorthy <nataraj.ramamoorthy13@gmail.com>
2025-04-17 14:03:32 +0530

    v2

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket  = "my-terraform-state-bobby"
+    bucket  = "my-terraform-state-ecs-demo"
     region  = "us-east-1"
     key     = "ecs-fargate/terraform.tfstate"
     encrypt = true
